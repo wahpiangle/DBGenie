cb98bc2e5a4450f04b2e22093de2526af93afc46
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2024-07-28 15:43:25 +0800

    add chat history

--- app/rag.py
+++ app/rag.py
@@ -3,11 +3,16 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_core.callbacks import CallbackManager
 from langchain import hub
 from langchain_community.llms import Ollama
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
 from vector_store import vectorstore
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 bs4_strainer = bs4.SoupStrainer()
 loader = WebBaseLoader(
@@ -20,23 +25,83 @@ splits = text_splitter.split_documents(docs)
 
 vectorstore.add_documents(splits)
 
-# Retrieve and generate using the relevant snippets of the blog.
 retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
+
+# chat history prompt
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+# Retrieve and generate using the relevant snippets of the blog.
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
 
 llm = Ollama(
     model="llama3.1:8b",
     callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
 )
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
+# add history aware retriever
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
 )
 
-llm.invoke("what does jina ai do")
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(
+    history_aware_retriever,
+    question_answer_chain,
+)
+
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+conversational_rag_chain.invoke(
+    {"input": "What does Jina AI do?"},
+    config={"configurable": {"session_id": "abc123"}},
+)["answer"]
+
+conversational_rag_chain.invoke(
+    {"input": "Please tell me more about it?"},
+    config={"configurable": {"session_id": "abc123"}},
+)["answer"]
