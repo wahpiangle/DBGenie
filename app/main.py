a8d5f69bd51d6d0b82763f6376a1055b34dace2e
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2024-08-10 19:49:20 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from contextlib import asynccontextmanager
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI
 
 from app.api.v1.routes import routers as v1_router
 from app.utils.init_db import create_tables
@@ -9,8 +8,3 @@ app = FastAPI()
 create_tables()
 
 app.include_router(v1_router, prefix="/v1")
-
-# @app.get("/chats/", response_model=list[ChatSchema.Chat])
-# def read_chats(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     chats = get_chats(db, skip=skip, limit=limit)
-#     return chats
