3c544eca8f38a059d589cbc27f525fcb4c1b2354
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2024-09-01 00:29:49 +0800

    load document function

--- app/vector_store.py
+++ app/vector_store.py
@@ -1,10 +1,13 @@
 # Setup Embeddings
+from pathlib import Path
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_postgres import PGVector
 from dotenv import load_dotenv
 import os
+from langchain_unstructured import UnstructuredLoader
+
 load_dotenv() 
- 
+
 modelPath = "Alibaba-NLP/gte-base-en-v1.5"
 model_kwargs = { "device": "cpu", "trust_remote_code": True }
 encode_kwargs = {'normalize_embeddings': True}
@@ -20,3 +23,10 @@ vectorstore = PGVector(
     # postgresql+psycopg://postgres:admin@localhost:5432/fyp
     connection=os.getenv("POSTGRES_URL"),
 )
+
+def load_document(file_name: str):
+    storage_folder = Path(__file__).parent / "storage"
+    file_to_open = storage_folder / file_name
+    loader = UnstructuredLoader(file_to_open)
+    docs = loader.load()
+    vectorstore.add_documents(docs)
