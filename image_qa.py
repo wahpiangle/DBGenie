cf73db086f717fd2bcf100b90b230d60369f1ea2
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2024-09-08 03:18:40 +0800

    Update image_qa.py

--- image_qa.py
+++ image_qa.py
@@ -6,8 +6,8 @@ model = AutoModel.from_pretrained('openbmb/MiniCPM-V-2_6-int4', trust_remote_cod
 tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-V-2_6-int4', trust_remote_code=True)
 model.eval()
 
-image = Image.open('demo.jpeg').convert('RGB')
-question = 'What is in the image?'
+image = Image.open('test.jpg').convert('RGB')
+question = 'When is the invoice number?'
 msgs = [{'role': 'user', 'content': [image, question]}]
 
 res = model.chat(
@@ -17,18 +17,18 @@ res = model.chat(
 )
 print(res)
 
-## if you want to use streaming, please make sure sampling=True and stream=True
-## the model.chat will return a generator
-res = model.chat(
-    image=None,
-    msgs=msgs,
-    tokenizer=tokenizer,
-    sampling=True,
-    temperature=0.7,
-    stream=True
-)
+# ## if you want to use streaming, please make sure sampling=True and stream=True
+# ## the model.chat will return a generator
+# res = model.chat(
+#     image=None,
+#     msgs=msgs,
+#     tokenizer=tokenizer,
+#     sampling=True,
+#     temperature=0.7,
+#     stream=True
+# )
 
-generated_text = ""
-for new_text in res:
-    generated_text += new_text
-    print(new_text, flush=True, end='')
+# generated_text = ""
+# for new_text in res:
+#     generated_text += new_text
+#     print(new_text, flush=True, end='')
