954a121d29021b13dc77873fd5bb2eca1ee821e4
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2025-01-03 16:26:38 +0800

    update

--- frontend/components/chatbox/chatbox.tsx
+++ frontend/components/chatbox/chatbox.tsx
@@ -7,13 +7,9 @@ import {
 import { Button } from "@/components/ui/button"
 
 import { Input } from "@/components/ui/input"
-import { AttachFileButton } from "./attach-file-btn"
-import RecordAudioButton from "./record-audio-btn";
 import { useState } from "react";
 
 export default function Chatbox() {
-    const [isRecording, setIsRecording] = useState(false)
-    const [hasRecording, setHasRecording] = useState(false)
     const [inputText, setInputText] = useState('')
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputText(e.target.value)
@@ -22,25 +18,17 @@ export default function Chatbox() {
         <div
             className="flex p-4 dark:border-none border md:w-9/12 w-11/12 rounded-lg dark:bg-darkTertiary items-center gap-2 justify-between"
         >
-            <AttachFileButton />
             <Input
                 id="message"
                 placeholder="Type your message here..."
                 className={`shadow-none dark:bg-darkSecondary border-none focus:border-none transition-none
-                        ${isRecording || hasRecording ? 'hidden' : 'block'}
                     `}
                 onInput={handleInput}
             />
-            {
-                inputText.length === 0 ? (
-                    <RecordAudioButton isRecording={isRecording} setIsRecording={setIsRecording} hasRecording={hasRecording} setHasRecording={setHasRecording} />
-                ) : (
-                    <Button type="button" className="">
-                        <SendHorizonalIcon className="size-4" />
-                        Send
-                    </Button>
-                )
-            }
+            <Button type="button" className="">
+                <SendHorizonalIcon className="size-4" />
+                Send
+            </Button>
         </div>
     )
 }
